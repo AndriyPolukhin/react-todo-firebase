50e783acc1ed243b270ad239777ff2e38e37d2b9
AndriyPolukhin <andriy.polukhin@gmail.com>
2020-09-01 20:57:39 +0300

    full crud added with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,46 @@
-import React, { useState } from 'react';
-import {
-  Button,
-  FormControl,
-  FormHelperText,
-  Input,
-  InputLabel,
-} from '@material-ui/core';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import { db } from './firebase';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import Todo from './Todo';
+import { AddCircleRounded } from '@material-ui/icons';
+import { yellow, grey } from '@material-ui/core/colors';
+import './App.css';
 
 const App = () => {
-  const [todos, setTodos] = useState([
-    'Buy Tickets to Berlin',
-    'Get a negative COVID test',
-    'Meet Anastasia in Berlin',
-  ]);
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
+
+  //  * When the app loads, we need to listen to the database and fetch new todos as they get added/removed
+  useEffect(() => {
+    // * this code here... fires when the app.js loads
+    db.collection('todos')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot((snapshot) => {
+        setTodos(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            todo: doc.data().todo,
+          }))
+        );
+      });
+  }, []);
 
   const addTodo = (event) => {
     event.preventDefault();
-    setTodos([...todos, input]);
+
+    db.collection('todos').add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
     setInput('');
   };
 
   return (
     <div className="App">
-      <h1>Hello, you coder</h1>
+      <h1>List of things to Todo!</h1>
       <form>
         <FormControl>
           <InputLabel>Enter a Todo</InputLabel>
@@ -41,13 +56,15 @@ const App = () => {
           disabled={!input}
           onClick={addTodo}
         >
-          Add Todo
+          <AddCircleRounded
+            style={!input ? { color: grey[500] } : { color: yellow[500] }}
+          />
         </Button>
       </form>
 
       <ul>
-        {todos.map((todo, i) => (
-          <Todo text={todo} key={i} />
+        {todos.map((todo) => (
+          <Todo todo={todo} key={todo.id} id={todo.id} />
         ))}
       </ul>
     </div>
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,7 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { db } from './firebase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  Modal,
+  Button,
+  Input,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+//  * Icons
 
-const Todo = ({ text }) => {
-  return <div>{text}</div>;
+import {
+  ImageRounded,
+  SaveRounded,
+  EditRounded,
+  DeleteSweepRounded,
+} from '@material-ui/icons';
+import { red, blue, green } from '@material-ui/core/colors';
+
+import './Todo.css';
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    width: 400,
+    border: '2px solid #000',
+    backgroundColor: 'white',
+    boxShadow: theme.shadows[3],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+const Todo = ({ todo: { todo }, id }) => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState('');
+
+  const updateTodo = () => {
+    //  * update the todo with the new input text
+    db.collection('todos').doc(id).set(
+      {
+        todo: input,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      },
+      { merge: true }
+    );
+    // * Close modal
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Modal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        className={classes.modal}
+      >
+        <div className={classes.paper}>
+          <h1>I am a model</h1>
+          <Input
+            placeholder={todo}
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button>
+            <SaveRounded onClick={updateTodo} style={{ color: green[500] }} />
+          </Button>
+        </div>
+      </Modal>
+      <List className="todo__list">
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <ImageRounded />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={todo} secondary="Dummy deadline" />
+
+          <EditRounded
+            onClick={(e) => setOpen(true)}
+            style={{ color: blue[500] }}
+          />
+          <DeleteSweepRounded
+            onClick={(event) => db.collection('todos').doc(id).delete()}
+            style={{ color: red[500] }}
+          />
+        </ListItem>
+      </List>
+    </>
+  );
 };
 
 export default Todo;
